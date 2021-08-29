71a9511a08c1089c9a8e77c441cea245e8c68f7b
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-08-29 20:47:52 +0530

    learn scope of variable and function

--- function_and_array/function/index.js
+++ function_and_array/function/index.js
@@ -15,3 +15,36 @@ sum(2);//here variable a take the value 2 and for b their is no value passed so 
 
 //--->>>>passing more arguments in function
 sum(2, 3, 5);//function simply ignore the extra value a=2,b=3;
+
+
+//scope 
+var name = "Dipankar"//---> variable which created globaly is global variable 
+
+function scopeDemo() {
+    // i is accesible within function scopeDemo
+    var i = 10;
+    console.log(i);
+}
+scopeDemo();
+//console.log(i)// i is accesible here cz it's not creted in global execution context
+
+//note--->>>>when interpreter found a variable it's first  check within the function(or within that execution context)
+
+//execution context--->we can say envoirment
+//global execution have variable envoirment and this
+//loca execution have variable envoirment ,this and reference to outer envoirment
+
+//execution stack--->store all execution context
+function c() {
+    console.log("inside c");
+}
+function b() {
+    c();
+    console.log("inside b");
+}
+function a() {
+    b();
+    console.log("inside a");
+}
+a();
+console.log("gloabal context");
